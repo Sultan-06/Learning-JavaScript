20e749571fcdba36968fbc6893d63269cf3cd406
Sultan-06 <zakinaik000@gmail.com>
2026-09-23 23:13:40 +0530

    learned variables and objects

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 console.log("Hello, World!");
-console.log("Hello, World!");
-console.log("Hello, World!");
-console.log("Hello, World!");
-console.log("Hello, World!");
-console.log("Hello, World!");
-console.log("Hello, World!");
-console.log("Hello, World!");
-console.log("Hello, World!");
-console.log("Hello, World!");
-console.log("Hello, World!");
-console.log("Hello, World!");
-console.log("Hello, World!");
+
+fullName = "tony stark";
+console.log(fullName);
+
+age = 48;
+console.log(age);
+
+x = null;
+console.log(x);
+
+y = undefined;
+console.log(y);
+
+
+const student = {
+    fullname : "tony stark",
+    age : 19,
+    cgpa : 8.2
+};
+
+console.log(student);
